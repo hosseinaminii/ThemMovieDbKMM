1aec4c61731cc0e2f73b47c8b2492eb84981678a
Hossein Amini <hosseinamiinii@gmail.com>
2021-04-02 14:53:33 +0430

    Init

---
+++ shared/src/commonMain/kotlin/com/example/themoviedb/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.example.themoviedb.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/themoviedb/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.themoviedb.shared
+
+expect class Platform() {
+    val platform: String
+}
